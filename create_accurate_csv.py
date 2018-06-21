68f292fee603a73c9e8d0bfce973a30b43182241
viraat <s1791387@ed.ac.uk>
2018-06-21 15:12:00 +0100

    updates

--- create_accurate_csv.py
+++ create_accurate_csv.py
@@ -18,6 +18,10 @@ def remove_missing_files(df, data_path):
 
     return df.drop(actors_frame.index[indices]), count
 
+def add_name_id(df):
+    df['person_id'] = pd.Categorical(pd.factorize(df.name)[0] + 1)
+    return df
+
 ANNOT_ACTORS_PATH = "data/facescrub_actors.txt"
 ANNOT_ACTRESS_PATH = "data/facescrub_actresses.txt"
 
@@ -31,12 +35,13 @@ actors_frame['gender'] = 'male'
 
 print('Before deletion, actors frame')
 print(actors_frame.head())
+print('Shape:', actors_frame.shape)
 
 updated_actors_frame, actors_count = remove_missing_files(actors_frame, DATA_ACTORS_PATH)
 
 print('After deletion, actors frame')
 print(updated_actors_frame.head())
-print(updated_actors_frame.shape)
+print('Shape:', updated_actors_frame.shape)
 print('{} lines were deleted'.format(actors_count))
 
 
@@ -45,19 +50,22 @@ actress_frame['gender'] = 'female'
 
 print('Before deletion, actress frame')
 print(actress_frame.head())
+print('Shape: ', actress_frame.shape)
 
 updated_actress_frame, actress_count = remove_missing_files(actress_frame, DATA_ACTRESS_PATH)
 
 print('After deletion, actress frame')
 print(updated_actress_frame.head())
-print(updated_actress_frame.shape)
+print('Shape:', updated_actress_frame.shape)
 print('{} lines were deleted'.format(actress_count))
 
 new_full_frame = updated_actors_frame.append(updated_actress_frame, ignore_index=True)
+print(new_full_frame.keys())
+full_frame_with_ids = add_name_id(new_full_frame)
 
-print(new_full_frame.head())
-print(new_full_frame.tail())
-print(new_full_frame.shape)
+print(full_frame_with_ids.head())
+print(full_frame_with_ids.tail())
+print('Shape:', full_frame_with_ids.shape)
 
-new_full_frame.to_csv(SAVE_PATH+'full_facescrub.txt', sep='\t', index=False)
-print('Successfully saved new dataframe to {}'.format(SAVE_PATH+'full_facescrub.txt'))
+full_frame_with_ids.to_csv(SAVE_PATH+'full_facescrub_with_ids.txt', sep='\t', index=False)
+print('Successfully saved new dataframe to {}'.format(SAVE_PATH+'full_facescrub_with_ids.txt'))
