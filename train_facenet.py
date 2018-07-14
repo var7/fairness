b6723ce007df1f3e9b63719031e51ffa0568356d
Viraat Aryabumi Reddy <viraat94@gmail.com>
2018-07-14 11:19:09 +0100

    added validation set loss function

--- train_facenet.py
+++ train_facenet.py
@@ -28,16 +28,24 @@ from utils import save_checkpoint, save_hyperparams
 ############## add parser arguments #############
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-j", "--job-number", dest="job_number", help="job number to store weights")
-parser.add_argument("-e", "--epochs", dest="epochs", default=20, type=int, help="Number of epochs")
-parser.add_argument("-r", "--resume-training", dest="resume", action="store_true", help="Resume training")
-parser.add_argument("-rw", "--weigths", dest="resume_weights", help="Path to weights file")
+parser.add_argument("-j", "--job-number", dest="job_number",
+                    help="job number to store weights")
+parser.add_argument("-e", "--epochs", dest="epochs",
+                    default=20, type=int, help="Number of epochs")
+parser.add_argument("-r", "--resume-training", dest="resume",
+                    action="store_true", help="Resume training")
+parser.add_argument("-rw", "--weigths", dest="resume_weights",
+                    help="Path to weights file")
 # parser.set_defaults(resume=False)
-parser.add_argument("-nc", "--no-cuda", dest='use_cuda', action='store_false', help="Do not use cuda")
+parser.add_argument("-nc", "--no-cuda", dest='use_cuda',
+                    action='store_false', help="Do not use cuda")
 # parser.set_defaults(use_cuda=True)
-parser.add_argument("-d", "--data-path", dest="data_path", help="path to data files")
-parser.add_argument("-bs", "--batch-size", default=128, dest="batch_size", type=int, help="batch size")
-parser.add_argument("-lr", "--learning-rate", default=1e-2, dest="learning_rate", type=float, help="learning rate")
+parser.add_argument("-d", "--data-path", dest="data_path",
+                    help="path to data files")
+parser.add_argument("-bs", "--batch-size", default=128,
+                    dest="batch_size", type=int, help="batch size")
+parser.add_argument("-lr", "--learning-rate", default=1e-2,
+                    dest="learning_rate", type=float, help="learning rate")
 
 args = parser.parse_args()
 print(args)
@@ -55,9 +63,12 @@ TRAIN_PATH = os.path.join(DATA_PATH, 'train_full_with_ids.txt')
 VALID_PATH = os.path.join(DATA_PATH, 'val_full_with_ids.txt')
 TEST_PATH = os.path.join(DATA_PATH, 'test_full_with_ids.txt')
 
-CURR_DATE = "{}_{}_{}00hrs".format(time.strftime("%b"), time.strftime("%d"), time.strftime("%H"))
-JOB_NUMBER = "{}_{}".format(args.job_number, CURR_DATE) if args.job_number is not None else CURR_DATE
-WEIGHTS_PATH = os.path.join(DATA_PATH, 'model_weigths', 'job_{}'.format(JOB_NUMBER))
+CURR_DATE = "{}_{}_{}00hrs".format(time.strftime(
+    "%b"), time.strftime("%d"), time.strftime("%H"))
+JOB_NUMBER = "{}_{}".format(
+    args.job_number, CURR_DATE) if args.job_number is not None else CURR_DATE
+WEIGHTS_PATH = os.path.join(
+    DATA_PATH, 'model_weigths', 'job_{}'.format(JOB_NUMBER))
 ############## hyper parameters #############
 batch_size = args.batch_size
 input_size = 299
@@ -67,13 +78,12 @@ num_epochs = args.epochs
 print_every = 50
 start_epoch = 0
 
-triplet_margin = 1.
-triplet_p = 2
+triplet_margin = 1.  # margin
+triplet_p = 2  # norm degree for distance calculation
 
 resume_training = args.resume
 ############## data loading #############
 cuda = False
-# if gpu is to be used
 if args.use_cuda and torch.cuda.is_available():
     device = torch.device("cuda")
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
@@ -102,64 +112,73 @@ data_transforms = {
 train_df = FaceScrubDataset(
     txt_file=TRAIN_PATH, root_dir=DATA_PATH, transform=data_transforms['train'])
 
+val_df = FaceScrubDataset(
+    txt_file=VALID_PATH, root_dir=DATA_PATH, transform=data_transforms['val'])
+
 print('Train data loaded from {}. Length: {}'.format(TRAIN_PATH, len(train_df)))
+print('Validation data loaded from {}. Length: {}'.format(VALID_PATH, len(val_df)))
+
 triplet_train_df = TripletFaceScrub(train_df, train=True)
-print('Train data converted to triplet data')
+print('Train data converted to triplet form. Length: {}'.format(len(triplet_train_df)))
 
+triplet_val_df=TripletFaceScrub(val_df, train=False)
+print('Validation data converted to triplet form. Length: {}'.format(
+    len(triplet_val_df)))
 
-triplet_dataloader = torch.utils.data.DataLoader(
-    triplet_train_df, batch_size=batch_size, shuffle=True, num_workers=1)
+train_tripletloader=torch.utils.data.DataLoader(
+    triplet_train_df, batch_size=batch_size, shuffle=True, num_workers=4)
+print('Train loader created. Length of train loader: {}'.format(
+    len(train_tripletloader)))
+
+val_tripletloader=torch.utils.data.DataLoader(
+    triplet_val_df, batch_size=batch_size, shuffle=True, num_workers=4)
+print('Val triplet loader created. Length of val load: {}'.format(
+    len(val_tripletloader)))
 
 ############## set up models #############
-inception = models.inception_v3(pretrained=True)
-inception.aux_logits = False
-num_ftrs = inception.fc.in_features
-inception.fc = nn.Linear(num_ftrs, output_dim)
+inception=models.inception_v3(pretrained=True)
+inception.aux_logits=False
+num_ftrs=inception.fc.in_features
+inception.fc=nn.Linear(num_ftrs, output_dim)
 
-tripletinception = TripletNet(inception)
+tripletinception=TripletNet(inception)
 
-params = list(tripletinception.parameters())
+params=list(tripletinception.parameters())
 print('Number of params in triplet inception: {}'.format(len(params)))
-
-
 ############## set up for training #############
-criterion = nn.TripletMarginLoss(margin=triplet_margin, p=triplet_p)
+print('Triplet margin: {}. Norm degree: {}.'.format(triplet_margin, triplet_p))
+criterion=nn.TripletMarginLoss(margin=triplet_margin, p=triplet_p)
 
-optimizer = optim.Adam(tripletinception.parameters(), lr=learning_rate)
-scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-
-num_epochs = num_epochs
-
-triplet_dataloader = torch.utils.data.DataLoader(
-    triplet_train_df, batch_size=batch_size, shuffle=True, num_workers=4)
-print('length of dataset {}'.format(len(triplet_train_df)))
-tripletinception.train()
-
+optimizer=optim.Adam(tripletinception.parameters(), lr=learning_rate)
+scheduler=lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+best_loss = 1e2
 if cuda:
     tripletinception.cuda()
-    print('sent model to gpu {}'.format(
+    print('Sent model to gpu {}'.format(
         next(tripletinception.parameters()).is_cuda))
 
-print('length of dataloader: {}'.format(len(triplet_dataloader)))
 ############## Load saved weights #############
 if resume_training:
-    resume_weights = args.resume_weights
+    resume_weights=args.resume_weights
     if cuda:
-        checkpoint = torch.load(resume_weights)
+        checkpoint=torch.load(resume_weights)
     else:
         # Load GPU model on CPU
-        checkpoint = torch.load(resume_weights,
+        checkpoint=torch.load(resume_weights,
                                 map_location=lambda storage,
                                 loc: storage)
 
-    start_epoch = checkpoint['epoch']
+    start_epoch=checkpoint['epoch']
     tripletinception.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
-    scheduler.load_state_dict(checkpoint['scheduler'])
-    print("=> loaded checkpoint '{}' (trained for {} epochs)".format(resume_weights, checkpoint['epoch']))
+    # scheduler.load_state_dict(checkpoint['scheduler'])
+    print("=> loaded checkpoint '{}' (trained for {} epochs)".format(
+        resume_weights, checkpoint['epoch']))
+    # for epoch in range(0, start_epoch):
+    #     scheduler.step()
 
 ############## Save Hyper params to file ############
-hyperparams = {
+hyperparams={
     'JOB_NUMBER': JOB_NUMBER,
     'batch_size': batch_size,
     'input_size': input_size,
@@ -176,44 +195,72 @@ hyperparams = {
 }
 save_hyperparams(hyperparams=hyperparams, path=WEIGHTS_PATH)
 ############## Training #############
+
 for epoch in range(start_epoch, start_epoch + num_epochs):
+    scheduler.step()
+
     tripletinception.train()
     print('Epoch {}/{}'.format(epoch, start_epoch + num_epochs - 1))
     print('-' * 10)
 
-    running_loss = 0.0
-    start_time = time.time()
-    for i, (imgs, labels) in enumerate(triplet_dataloader):
-        batch_start = time.time()
+    running_loss=0.0
+    losses=[]
+
+    start_time=time.time()
+    for batch_idx, (imgs, labels) in enumerate(train_tripletloader):
+        batch_start=time.time()
         optimizer.zero_grad()
-        imgs = [img.to(device) for img in imgs]
-        embed_anchor, embed_pos, embed_neg = tripletinception(
+        imgs=[img.to(device) for img in imgs]
+        embed_anchor, embed_pos, embed_neg=tripletinception(
             imgs[0], imgs[1], imgs[2])
 
         loss = criterion(embed_anchor, embed_pos, embed_neg)
 
+        running_loss += loss.item()
+        losses.append(loss.item())
+
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.item()
+        if batch_idx % print_every == 0:
+            batch_end=(time.time() - batch_start)
+            print('batch number: {} loss: {} time: {} sec'.format(
+                batch_idx, running_loss/(batch_idx+1), batch_end))
 
-        if i % print_every == 0:
-            batch_end = (time.time() - batch_start)/60
-            print('batch number: {} loss: {} time: {} min'.format(i, running_loss/(i+1), batch_end))
-           
-    elapsed = (time.time() - start_time)/60
-    print('Elapsed time for epoch {}: {} minutes'.format(epoch, elapsed))
+    val_loss = 0
+    val_losses = []
+    with torch.no_grad():
+        tripletinception.eval()
+        val_loss = 0
+        for batch_idx, (imgs, labels) in enumerate(val_tripletloader):
+            imgs=[img.to(device) for img in imgs]
+            embed_anchor, embed_pos, embed_neg=tripletinception(
+                imgs[0], imgs[1], imgs[2])
 
-    state = {
+            loss = criterion(embed_anchor, embed_pos, embed_neg)
+
+            val_loss += loss.item()
+            val_losses.append(loss.item())
+
+        val_loss = val_loss / (batch_idx + 1)
+
+    print('Validation loss: {}'.format(val_loss))
+
+    state={
         'epoch': epoch,
         'state_dict': tripletinception.state_dict(),
         'optimizer': optimizer.state_dict(),
-        'scheduler': scheduler.state_dict()
+        'train_losses': losses,
+        'val_losses': val_losses,
+        'best_loss': best_loss
+        # 'scheduler': scheduler.state_dict()
     }
+    if best_loss > val_loss:
+        best_loss = val_loss
+        MODEL_NAME=os.path.join(WEIGHTS_PATH, 'weights_{}.pth'.format(epoch))
+        save_checkpoint(state, True, WEIGHTS_PATH, MODEL_NAME)
 
-    MODEL_NAME = os.path.join(WEIGHTS_PATH, 'weights_{}.pth'.format(epoch))
-
-    #if epoch % 2 == 0:
-    save_checkpoint(state, True, WEIGHTS_PATH, MODEL_NAME)
+    elapsed=(time.time() - start_time)/60
+    print('Elapsed time for epoch {}: {} minutes'.format(epoch, elapsed))
 
 print('Finished training')
