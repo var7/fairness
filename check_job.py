673c4dc3982a4517258b4809f49fb6edeecdfa0e
viraat aryabumi <s1791387@sms.ed.ac.uk>
2018-07-14 10:21:07 +0100

    update check job script

--- check_job.py
+++ check_job.py
@@ -1,5 +1,15 @@
 import subprocess
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--sleep", dest='sleep_time', type=int, default=120, help="update interval in secs")
+
+args = parser.parse_args()
+
 bashCommand = "squeue -u s1791387"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-print(output)
+while(1):
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output.decode())
+    time.sleep(args.sleep_time)
