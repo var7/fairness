1d719e640056d98cf22de5dc5e132b3cc47c7a42
Viraat Aryabumi Reddy <viraat94@gmail.com>
2018-07-15 21:35:23 +0100

    changes

--- check_job.py
+++ check_job.py
@@ -6,17 +6,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--sleep", dest='sleep_time', type=int, default=120, help="update interval in secs")
 
 args = parser.parse_args()
-
-bashCommand = "squeue -u s1791387 -p Short"
+job_id = 0000
+state = []
+bashCommand = "squeue -u s1791387 -p Standard"
 while(1):
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    if output is None:
+        put_job(state)
+
     state = output.decode().split()
     print(state)
     job_id = state[8]
     print(job_id)
-    if output is None:
-        put_job(state)
+
     print(output.decode())
     time.sleep(args.sleep_time)
 
