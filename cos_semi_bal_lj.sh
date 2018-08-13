837e12e375a5f3da978a0df6f8b27eff8ae68f41
viraat aryabumi <s1791387@sms.ed.ac.uk>
2018-08-13 11:59:02 +0100

    add training scripts

---
+++ cos_semi_bal_lj.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16000  # memory in Mb
+#SBATCH -o ./bal_logs/ljcos_randsemi_%j.log  # send stdout to sample_experiment_outfile
+#SBATCH -e ./bal_logs/ljcos_randsemi_%j.err  # send stderr to sample_experiment_errfile
+#SBATCH -t 24:00:00  # time requested in hour:minute:secon
+#SBATCH -p LongJobs
+export CUDA_HOME=/opt/cuda-8.0.44
+
+export CUDNN_HOME=/opt/cuDNN-6.0_8.0
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+# Activate the relevant virtual environment:
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate fairness
+
+export XDG_RUNTIME_DIR=${TMPDIR}
+
+python balanced_facenet_train.py -j cosine6_randsemi_std -e 80 --num-classes 6 --num-samples 5 --cosine -r -rw /home/s1791387/facescrub-data/new_data_max/balanced_model_weigths/job_cosine5_semi_std_Jul_20_1900hrs/weights_65.pth
---
+++ openface.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16000  # memory in Mb
+#SBATCH -o ./openface_logs/cos_semi_std_%j.log  # send stdout to sample_experiment_outfile
+#SBATCH -e ./openface_logs/cos_semi_std_%j.err  # send stderr to sample_experiment_errfile
+#SBATCH -t 8:00:00  # time requested in hour:minute:secon
+#SBATCH -p Standard
+export CUDA_HOME=/opt/cuda-8.0.44
+
+export CUDNN_HOME=/opt/cuDNN-6.0_8.0
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+# Activate the relevant virtual environment:
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate fairness
+
+export XDG_RUNTIME_DIR=${TMPDIR}
+
+python openface_train.py -j rand_std_cos3 -e 50 --num-classes 15 --num-samples 20 --cosine -r -rw /home/s1791387/facescrub-data/new_data_max/balanced_model_weigths/job_rand_std_cos2_Jul_24_2300hrs/weights_31.pth
